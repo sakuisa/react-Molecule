af4629fb0b296db152c0c8cb2a4f8924e573feff
sakusan <sssaku.039@gmail.com>
2023-07-05 10:40:03 +0000

    Update 4 files

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,23 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img
+        height={160}
+        width={160}
+        src="https://source.unsplash.com/NE0XGVKTmcA"
+        alt="プロフィール"
+      />
+      <p>名前</p>
+      <dl>
+        <dt>mail</dt>
+        <dd>1111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-0000-9999</dd>
+        <dt>company</dt>
+        <dd>aiueo会社</dd>
+        <dt>WEB</dt>
+        <dd>httttt.com</dd>
+      </dl>
+    </div>
+  );
+};
