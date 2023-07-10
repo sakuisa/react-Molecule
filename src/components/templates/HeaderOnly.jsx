187716b6dba3560818f96136c9b2032410d6150b
sakusan <sssaku.039@gmail.com>
2023-07-10 10:54:03 +0000

    Update 6 files

---
+++ src/components/templates/HeaderOnly.jsx
@@ -0,0 +1,13 @@
+import { Header } from "../atoms/layout/Header";
+
+
+export const HeaderOnly = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
+
