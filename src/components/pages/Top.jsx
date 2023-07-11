4302d1c1828c9bf1180b21e64119bcf33ff5ca70
sakusan <sssaku.039@gmail.com>
2023-07-11 12:15:46 +0000

    Update 4 files

---
+++ src/components/pages/Top.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Top = () => {
+  return (
+    <SConainer>
+      <h2>TOPページです</h2>
+    </SConainer>
+  );
+};
+
+const SConainer = styled.div`
+  text-align: center;
+`;
